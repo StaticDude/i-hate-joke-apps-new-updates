c2b09bb10408e2f1d88bedcbf9a1801e37d0de2d
Coder rudra <83690766+xxdev-specs@users.noreply.github.com>
2021-09-29 19:39:44 +0530

    Create secrets.py

---
+++ secrets.py
@@ -0,0 +1,43 @@
+```python
+#added name secrets
+
+if name == "Rudra":
+  print("nice name buddy!!!")
+
+if name == "mynameisyourname":
+  print(".......")
+
+#also 1 new change
+
+print("types of coder: \n intern: always forgetting something \n junior coder the one who is learing \n senior coder doesnt codes any more also... i feel like they are lazy now a days in loackdown \n", name, "YES \n wth am i doing this should be a joke app NOOOO")
+
+#i know pretty long code the change was name and YES
+
+#and i added code password thingy
+
+ode = input("code: ")
+
+if code == '*********':       #follow me on dev.to/coderrudra to reveal the code
+  print(":oooooooo")
+
+print("")
+print("")
+print("")
+print("")
+print("")
+print("")
+print("")
+print("")
+print("")
+print("")
+print("")
+print("")
+print("")
+print("21")
+print("")
+print("")
+print(":|")
+
+#so these are the updates i added
+
+```
